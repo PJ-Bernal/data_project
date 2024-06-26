8f85b110fb7ae4762a437545140cae7d9c1d1ec0
Pablo Bernal <pj.bernal05@outlook.com>
2024-06-26 11:40:47 -0500

    Ailment, Location, Monster, MonstersLocation and MonstersAilment models created. Associations done. Migration done

---
+++ app/models/monster.rb
@@ -0,0 +1,8 @@
+class Monster < ApplicationRecord
+
+    has_many :monsters_locations
+    has_many :locations, through: :monsters_locations
+    has_many :monsters_ailments
+    has_many :ailments, through: :monsters_ailments
+    
+end
