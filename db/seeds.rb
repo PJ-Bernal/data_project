1f21a61d1f8542c0c49f15e1275c2cecf87059e3
Pablo Bernal <pj.bernal05@outlook.com>
2024-06-26 11:49:40 -0500

    Seeded done. All API data fetched correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,41 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'net/http'
+require 'json'
+
+def fetch_data(endpoint)
+  url = URI.parse("https://mhw-db.com/#{endpoint}")
+  response = Net::HTTP.get_response(url)
+  JSON.parse(response.body)
+end
+
+# Fetch and create monsters
+monsters_data = fetch_data('monsters')
+monsters_data.each do |monster|
+  Monster.create(
+    name: monster['name'],
+    size: monster['type'],
+    species: monster['species'],
+    description: monster['description']
+  )
+end
+
+# Fetch and create locations
+locations_data = fetch_data('locations')
+locations_data.each do |location|
+  Location.create(
+    name: location['name'],
+    zoneCount: location['zoneCount']
+  )
+end
+
+# Fetch and create ailments
+ailments_data = fetch_data('ailments')
+ailments_data.each do |ailment|
+  Ailment.create(
+    name: ailment['name'],
+    description: ailment['description']
+  )
+end
