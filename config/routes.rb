7b804cf8d14c7324e35131f2ee43457a0c74ee32
Pablo Bernal <pj.bernal05@outlook.com>
2024-06-27 13:56:28 -0500

    Added documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
   root 'monsters#index'
 
-  get '/index', to: 'monsters#index'
+  get '/monsters', to: 'monsters#index'
+
+  # Map the default id of the monster, to the monster's name 
+  resources :monsters, param: :name, only: [:index, :show]
+  get '/monsters/:name', to: 'monsters#show'
+
   get '/locations', to: 'locations#index'
+
   get '/ailments', to: 'ailments#index'
+
+
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
